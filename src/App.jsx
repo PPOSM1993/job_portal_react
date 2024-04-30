14f5d2110934cfc1354d5100186e5e317960c443
Administrador <administrador@ubuntudocker.myguest.virtualbox.org>
2024-04-30 10:57:50 -0400

    Se agregaron nuevos componentes a la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 import Hero from './components/Hero';
-import Card from './components/Card';
 import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+
 
 const App = () => {
   return (
   <>
-  <Navbar />
-  <Hero />
-  <Card />
-  <HomeCards />
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
   </>)
 };
 
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import jobs from '../jobs.json'
+
+const JobListings = () => {
+    console.log(jobs);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobListings
